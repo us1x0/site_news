4a2907f3cb79b4d20e9d422ad9b6760593b9899c
us1x0 <farkhodovicmsh@gmail.com>
2024-10-27 20:36:12 +0500

    Добаления html ФАЙЛЫ и ADMIN панель

--- novostnoysayt_app/admin.py
+++ novostnoysayt_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import News, OptionsNews
+
 
 # Register your models here.
+admin.site.register(News)
+admin.site.register(OptionsNews)
+
--- novostnoysayt_app/models.py
+++ novostnoysayt_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.forms import CharField
+
 
 # Create your models here.
+class News(models.Model):
+    which_news = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.which_news
+
+
+class OptionsNews(models.Model):
+    news_zagalovok = models.CharField(max_length=128)
+    news = models.CharField(max_length=1024)
+    news_who_write = CharField(max_length=64)
+    news_photo = models.ImageField(blank=True, null=True)
+    news_date_time = models.CharField(max_length=32)
+    news_category = models.ForeignKey(News, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.news_zagalovok
+
+
+
+
